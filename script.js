5c63b759399ea66a838f1fd4f10f86f480eb5acb
jhall9871 <james@jameshallmusic.com>
2020-06-08 13:58:03 -0500

    added API connection

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function dropDown() {
-    document.getElementById("myDropdown").classList.toggle("show");
+$(() => {
+  console.log("app.js", $);
+
+  let $div = $("div");
+
+  let sheetUrl =
+    "https://docs.google.com/spreadsheets/d/1N0Lh8_H1klacQsz1raOYf-k9fTBUUEsk1rrIgE8Spos/edit?usp=sharing";
+  let sheetAsJSON =
+    "https://spreadsheets.google.com/feeds/list/1N0Lh8_H1klacQsz1raOYf-k9fTBUUEsk1rrIgE8Spos/od6/public/values?alt=json";
+
+  $.ajax({ url: sheetAsJSON }).then((data) => {
+    console.log("data", data);
+
+    let projects = data.feed.entry.map((project) => {
+      return {
+        title: project.gsx$title.$t,
+        image: project.gsx$image.$t,
+        description: project.gsx$description.$t,
+        url: project.gsx$url.$t,
+      };
+    });
+    app(projects);
+  });
+  //this function is called from within the ajax method so that it has access to the projects variable. All the stuff you want to do should be in here.
+  function app(projects) {
+    // for (let i = 0; i < projects.length; i++) {
+    //   let imgUrl = "url('" + projects[i].image + "')";
+    //   $("#" + i).css("background-image", imgUrl);
+    // }
+
+    projects.forEach((project, index) => {
+      let imgUrl = "url('" + projects[index].image + "')";
+      let $div = 
+      $('#portfolio').append($('<div>').addClass('project-tile').css("background-image", imgUrl))
+    })
+
   }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
+});
